651e53e8ae2b0e26c2dc7858a9a71fc4372a3c87
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-04-30 16:05:22 +0700

    Update telegram-web-app.js

--- telegram-web-app.js
+++ telegram-web-app.js
@@ -4,7 +4,6 @@ tg.expand();
 create_training.addEventListener("click", () => {
 	let date_training = document.getElementById("id_date_training").value;
 	let time_training = document.getElementById("id_time_training").value;
-	let hidden_survey = document.getElementById("id_hidden_survey").value;
 	let location = document.getElementById("id_location").value;	
 	let type_traning = document.getElementById("id_type_traning").value;
 	let limit_number_players = document.getElementById("id_limit_number_players").value;
@@ -13,7 +12,6 @@ create_training.addEventListener("click", () => {
 	let data = {
 		date_training: date_training,
 		time_training: time_training,
-		hidden_survey: hidden_survey,
 		location: location,
 		type_traning: type_traning,
 		limit_number_players: limit_number_players,
